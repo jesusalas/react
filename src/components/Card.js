d19d5efdc65a07815e6da2068c532ad37f59e167
jesus salas <jesusalas18@gmail.com>
2019-09-01 17:29:42 -0600

    release (fitness - proyecto): se creo el proyecto de reac fitness

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import exerciseImg from '../images/exercise.png'
+
+class Card  extends React.Component {
+   render() {
+       return (
+           <div>
+               <div>
+                <img src={exerciseImg}/>
+               </div>
+               <div>
+                   <h1>Technique Guides</h1>
+                   <p>Learn amazing street workout and calit</p>
+               </div>
+           </div>
+       
+        )
+   }
+}
+
+export default Card
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/Card'
+
+const container = document.getElementById('root')
+
+ReactDOM.render(< Card/>, container);
+    
