009856cbc7c43cdd85eec1170e6f49c9168c8342
jesus salas <jesusalas18@gmail.com>
2019-09-01 18:02:06 -0600

    (fitness): agregando css a componentes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const container = document.getElementById('root')
 
 ReactDOM.render(< Card/>, container);
-    
