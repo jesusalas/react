0090818345ce9c6d1f88011e2e7655b7d3c7b920
Jesus <jesusalas18@gmail.com>
2019-09-05 14:20:07 -0600

    (react): componentes funcionales

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card'
+import Welcome from './components/Welcome'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(< Card
-                title="Technique Guides"
-                description="Learn amazing street workout and calit"
-                leftColor="#A74CF2"
-                rightColor="#617BFB"
-                />, container);
+ReactDOM.render(<div>
+                    <Welcome
+                        username="Jesus"
+                    />
+                    < Card
+                        title="Technique Guides"
+                        description="Learn amazing street workout and calit"
+                        leftColor="#A74CF2"
+                        rightColor="#617BFB"
+                    />
+                </div>, container);
