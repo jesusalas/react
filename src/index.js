908697a9e2c3fefda9d0db3ee1ab05e2e1e36d22
Jesus <jesusalas18@gmail.com>
2019-09-06 15:00:15 -0600

    (react): enrutado dinamico de componentes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Exercises from './pages/Exercises'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './components/App'
+
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Exercises/>, container);
+ReactDOM.render(<App/>, container);
