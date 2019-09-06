eb026626db9c1a112d645dd7a3cd9fe97ebc1ce5
Jesus <jesusalas18@gmail.com>
2019-09-06 14:12:44 -0600

    (react): listando componentes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import exerciseImg from '../images/exercise.png'
 import './styles/Card.css'
 
 class Card  extends React.Component {
@@ -14,7 +13,7 @@ class Card  extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exerciseImg}className="float-right" />
+                            <img src={img} className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
